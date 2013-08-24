722649636cf3f72fb6a5eee4f90dc3c6e8e4045f
Rachel Wall <solistra@gmx.com>
2013-08-23 21:49:39 -0400

    Minor reformatting.

--- README.md
+++ README.md
@@ -4,17 +4,17 @@ Core v2.0 by Solistra
 
 Summary
 ------------------------------------------------------------------------------
-This script provides the basic foundation for all SES scripts written by
+  This script provides the basic foundation for all SES scripts written by
 Enelvon and Solistra. It provides necessary infrastructure and an assortment
 of utility methods and extensions to the defaults provided by RPG Maker VX
 Ace.
 
 License
 ------------------------------------------------------------------------------
-This script is made available under the terms of the MIT Expat license. Visit
+  This script is made available under the terms of the MIT Expat license. View
 [this page](http://sesvxace.wordpress.com/license/) for more information.
 
 Installation
 ------------------------------------------------------------------------------
-Place this script below Materials, but above Main and all other SES scripts.
+  Place this script below Materials, but above Main and all other SES scripts.
 
--- core.rb
+++ core.rb
@@ -4,19 +4,19 @@
 # 
 # Summary
 # ------------------------------------------------------------------------------
-# This script provides the basic foundation for all SES scripts written by
+#   This script provides the basic foundation for all SES scripts written by
 # Enelvon and Solistra. It provides necessary infrastructure and an assortment
 # of utility methods and extensions to the defaults provided by RPG Maker VX
 # Ace.
 # 
 # License
 # ------------------------------------------------------------------------------
-# This script is made available under the terms of the MIT Expat license. Visit
+#   This script is made available under the terms of the MIT Expat license. View
 # [this page](http://sesvxace.wordpress.com/license/) for more information.
 # 
 # Installation
 # ------------------------------------------------------------------------------
-# Place this script below Materials, but above Main and all other SES scripts.
+#   Place this script below Materials, but above Main and all other SES scripts.
 # 
 #++
 module SES
