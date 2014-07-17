7ba30b2ae20db8adda539038dac80592c95cb107
Rachel Wall <solistra@gmx.com>
2014-07-16 20:55:12 -0400

    Provide namespace for unit tests, update error testing.

--- test/test_core_method_data.rb
+++ test/test_core_method_data.rb
@@ -1,59 +1,58 @@
 module SES::TestCases
-  # ===========================================================================
-  # CoreMethodDataTest - Unit tests for the SES::MethodData module.
-  # ===========================================================================
-  class CoreMethodDataTest < SES::Test::Spec
-    describe 'MethodData' do SES::MethodData end
-    
-    def clean(type, key)
-      subject.send(type).delete(key)
-    end
-    
-    it '.register_alias registers aliases appropriately' do
-      subject.register_alias(self, :test_clean, :clean)
-      subject.aliases.must_include self
-      subject.aliases[self][:clean].must_include :test_clean
+  module Core
+    class MethodDataTest < SES::Test::Spec
+      describe 'MethodData' do SES::MethodData end
       
-      clean(:aliases, self)
-    end
-    
-    it '.register_alias returns true with new alias' do
-      subject.register_alias(self, :test_clean, :clean).must_equal true
+      def clean(type, key)
+        subject.send(type).delete(key)
+      end
       
-      clean(:aliases, self)
-    end
-    
-    it '.register_alias returns false without new alias' do
-      subject.register_alias(self, :test_clean, :clean)
-      subject.register_alias(self, :test_clean, :clean).must_equal false
+      it '.register_alias registers aliases appropriately' do
+        subject.register_alias(self, :test_clean, :clean)
+        subject.aliases.must_include self
+        subject.aliases[self][:clean].must_include :test_clean
+        
+        clean(:aliases, self)
+      end
       
-      clean(:aliases, self)
-    end
-    
-    it '.register_alias ignores Test Case stubbed object aliases' do
-      object = Object.new
-      object.stub(:object_id, 0) { subject.aliases.cannot_include object }
-    end
-    
-    it '.register_overwrite registers overwrites appropriately' do
-      subject.register_overwrite(self, :clean)
-      subject.overwrites.must_include self
-      subject.overwrites[self].must_include :clean
+      it '.register_alias returns true with new alias' do
+        subject.register_alias(self, :test_clean, :clean).must_equal true
+        
+        clean(:aliases, self)
+      end
       
-      clean(:overwrites, self)
-    end
-    
-    it '.register_overwrite returns true with new overwrite' do
-      subject.register_overwrite(self, :clean).must_equal true
+      it '.register_alias returns false without new alias' do
+        subject.register_alias(self, :test_clean, :clean)
+        subject.register_alias(self, :test_clean, :clean).must_equal false
+        
+        clean(:aliases, self)
+      end
       
-      clean(:overwrites, self)
-    end
-    
-    it '.register_overwrite returns false without new overwrite' do
-      subject.register_overwrite(self, :clean)
-      subject.register_overwrite(self, :clean).must_equal false
+      it '.register_alias ignores Test Case stubbed object aliases' do
+        object = Object.new
+        object.stub(:object_id, 0) { subject.aliases.cannot_include object }
+      end
       
-      clean(:overwrites, self)
+      it '.register_overwrite registers overwrites appropriately' do
+        subject.register_overwrite(self, :clean)
+        subject.overwrites.must_include self
+        subject.overwrites[self].must_include :clean
+        
+        clean(:overwrites, self)
+      end
+      
+      it '.register_overwrite returns true with new overwrite' do
+        subject.register_overwrite(self, :clean).must_equal true
+        
+        clean(:overwrites, self)
+      end
+      
+      it '.register_overwrite returns false without new overwrite' do
+        subject.register_overwrite(self, :clean)
+        subject.register_overwrite(self, :clean).must_equal false
+        
+        clean(:overwrites, self)
+      end
     end
   end
 end
--- test/test_core_script.rb
+++ test/test_core_script.rb
@@ -1,51 +1,50 @@
 module SES::TestCases
-  # ===========================================================================
-  # CoreScriptTest - Unit tests for the SES::Script class.
-  # ===========================================================================
-  class CoreScriptTest < SES::Test::Spec
-    describe 'Script' do SES::Script end
-    
-    it 'initializes with given name' do
-      subject.new(:Example).name.must_be_same_as :Example
-    end
-    
-    it 'defines #authors, #name, and #version methods' do
-      script = subject.new(:Example)
-      [:authors, :name, :version].each do |instance_method|
-        script.must_respond_to(instance_method)
+  module Core
+    class CoreScriptTest < SES::Test::Spec
+      describe 'Script' do SES::Script end
+      
+      it 'initializes with given name' do
+        subject.new(:Example).name.must_be_same_as :Example
       end
-    end
-    
-    it '#authors, #name, and #version return expected values' do
-      script = subject.new(:Example)
-      [:authors, :name, :version].each do |ivar|
-        expectation = script.instance_variable_get('@data')[ivar]
-        script.send(ivar).must_equal expectation
+      
+      it 'defines #authors, #name, and #version methods' do
+        script = subject.new(:Example)
+        [:authors, :name, :version].each do |instance_method|
+          script.must_respond_to(instance_method)
+        end
       end
-    end
-    
-    it 'initializes with default author and version' do
-      script = subject.new(:Example)
-      script.authors.must_equal [:Solistra, :Enelvon]
-      script.version.must_equal 1.0
-    end
-    
-    it 'initializes with given version and authors' do
-      script = subject.new(:Example, 0.1, :Nobody)
-      script.authors.must_equal [:Nobody]
-      script.version.must_equal 0.1
-    end
-    
-    it '.format appropriately formats symbols' do
-      subject.format(:Example_Script).must_be_same_as :"Example Script"
-    end
-    
-    it '.format appropriately formats descriptive symbols' do
-      subject.format(:"Example Script").must_be_same_as :Example_Script
-    end
-    
-    it '.format appropriately formats descriptive strings' do
-      subject.format('Example Script').must_be_same_as :Example_Script
+      
+      it '#authors, #name, and #version return expected values' do
+        script = subject.new(:Example)
+        [:authors, :name, :version].each do |ivar|
+          expectation = script.instance_variable_get('@data')[ivar]
+          script.send(ivar).must_equal expectation
+        end
+      end
+      
+      it 'initializes with default author and version' do
+        script = subject.new(:Example)
+        script.authors.must_equal [:Solistra, :Enelvon]
+        script.version.must_equal 1.0
+      end
+      
+      it 'initializes with given version and authors' do
+        script = subject.new(:Example, 0.1, :Nobody)
+        script.authors.must_equal [:Nobody]
+        script.version.must_equal 0.1
+      end
+      
+      it '.format appropriately formats symbols' do
+        subject.format(:Example_Script).must_be_same_as :"Example Script"
+      end
+      
+      it '.format appropriately formats descriptive symbols' do
+        subject.format(:"Example Script").must_be_same_as :Example_Script
+      end
+      
+      it '.format appropriately formats descriptive strings' do
+        subject.format('Example Script').must_be_same_as :Example_Script
+      end
     end
   end
 end
